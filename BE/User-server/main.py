539d08aeba86bc59d327e9a00dfc13e78b9e1c6a
itdice <itdiceofficial@gmail.com>
2025-01-15 13:59:58 +0900

    :card_file_box: [Mod] Dev Branch 생성 후 FastAPI 기본구조 구축

---
+++ BE/User-server/main.py
@@ -0,0 +1,21 @@
+"""
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ Care-bot User API Server ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+version : 0.0.1
+"""
+
+# Libraries
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
