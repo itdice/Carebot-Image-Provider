0d6bea352502f6cf56da9d9fef0ec99628f46569
itdice <itdiceofficial@gmail.com>
2025-01-17 13:54:55 +0900

    :sparkles: [Feat] 계정 생성 기능 구현

--- BE/User-server/Database/connector.py
+++ BE/User-server/Database/connector.py
@@ -50,14 +50,44 @@ class Database:
     def get_all_email(self) -> list[dict]:
         """
         등록된 사용자 계정 이메일을 모두 불러오는 기능
-        :return: 등록된 사용자의 모든 이메일 list
+        :return: 등록된 사용자의 모든 이메일 list[dict]
         """
         try:
-            account_list = self.session.query(Account.email).all()
+            account_list = self.session.query(AccountTable.email).all()
             return [{"email": data[0]} for data in account_list]
-
         except SQLAlchemyError as error:
             print(f"[DB] Error getting all email: {str(error)}")
             return []
         finally:
             self.session.close()
+
+    # 모든 사용자의 ID 불러오기
+    def get_all_account_id(self) -> list[dict]:
+        """
+        등록된 사용자 계정 ID를 모두 불러오는 기능
+        :return: 등록된 사용자의 모든 ID list[dict]
+        """
+        try:
+            id_list = self.session.query(AccountTable.id).all()
+            return [{"id": data[0]} for data in id_list]
+        except SQLAlchemyError as error:
+            print(f"[DB] Error getting all account id: {str(error)}")
+            return []
+        finally:
+            self.session.close()
+
+    # 새로운 사용자 계정 추가하기
+    def create_account(self, account_data: AccountTable) -> bool:
+        try:
+            self.session.add(account_data)
+            self.session.commit()
+            print(f"[DB] New account created: {account_data}")
+            return True
+        except SQLAlchemyError as error:
+            self.session.rollback()
+            print(f"[DB] Error creating new account: {str(error)}")
+            return False
+        finally:
+            self.session.close()
+
+
--- BE/User-server/main.py
+++ BE/User-server/main.py
@@ -2,22 +2,135 @@
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 ┃ Care-bot User API Server ┃
 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-version : 0.0.1
+version : 0.0.2
 """
 
 # Libraries
 from fastapi import FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+
 from Database.connector import Database
+from Database.models import *
+
 from Endpoint.data_blocks import *
+
+from utilities import *
+from datetime import date
 
 app = FastAPI()
 database = Database()
 
 
 # 새로운 계정을 생성하는 기능
-@app.post("/accounts")
+@app.post("/accounts", status_code=status.HTTP_201_CREATED)
 async def create_account(account: Account):
-    pass
+    # 필수 입력 정보 점검 (비밀번호, 역할, 이메일)
+    if account.password == "":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "no data",
+                "loc": ["body", "password"],
+                "message": "Password is required",
+                "input": jsonable_encoder(account)
+            }
+        )
+    elif account.role == "":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "no data",
+                "loc": ["body", "role"],
+                "message": "Role is required",
+                "input": jsonable_encoder(account)
+            }
+        )
+    elif account.email == "":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "no data",
+                "loc": ["body", "email"],
+                "message": "Email is required",
+                "input": jsonable_encoder(account)
+            }
+        )
+
+    # 중복 이메일 점검
+    email_list = [data["email"] for data in database.get_all_email()]
+
+    if account.email in email_list:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={
+                "type": "already exists",
+                "message": "Email is already in use",
+                "input": jsonable_encoder(account)
+            }
+        )
+
+    # ID 생성 및 중복 점검
+    new_id = ""
+    id_verified = False
+
+    while not id_verified:
+        new_id = random_id(16, Identify.USER)
+        id_list = [data["id"] for data in database.get_all_account_id()]
+        if new_id not in id_list:
+            id_verified = True
+
+    # 새로운 Account 정보 생성
+    hashed_password = hash_password(account.password)  # 암호화된 비밀번호
+    if account.birth_date is None:  # 입력받은 생년월일을 date 타입으로 변환
+        converted_birth_date = None
+    else:
+        converted_birth_date = date(
+            year=account.birth_date.year,
+            month=account.birth_date.month,
+            day=account.birth_date.day
+        )
+
+    try:
+        new_account = AccountTable(
+            id=new_id,
+            email=account.email,
+            password=hashed_password,
+            role=account.role.upper(),
+            user_name=account.user_name,
+            birth_date=converted_birth_date,
+            gender=account.gender.upper(),
+            address=account.address
+        )
+
+        # 계정 업로드
+        result = database.create_account(new_account)
+        if result:
+            return {
+                "message": "New account created successfully",
+                "id": new_id
+            }
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail={
+                    "type": "server error",
+                    "message": "Failed to create new account",
+                    "input": jsonable_encoder(account)
+                }
+            )
+    except LookupError as error:
+        database.session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "invalid value",
+                "message": "Invalid value provided for account details (role or gender)",
+                "input": jsonable_encoder(account)
+            }
+        )
+
+
+
 
 
 # 가입 가능한 이메일인지 확인하는 기능
@@ -30,14 +143,18 @@ async def check_email(email_check: EmailCheck):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={
-                "message": "Email is required"
+                "type": "no data",
+                "message": "Email is required",
+                "input": jsonable_encoder(email_check)
             }
         )
     elif target_email in email_list:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail={
-                "message": "Email is already in use"
+                "type": "already exists",
+                "message": "Email is already in use",
+                "input": jsonable_encoder(email_check)
             }
         )
     else:
