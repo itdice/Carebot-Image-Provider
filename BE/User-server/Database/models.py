2e1030d5377e196223ff293ef39734dcf17da500
itdice <itdiceofficial@gmail.com>
2025-01-17 13:52:05 +0900

    :zpa: [Perf] 나이를 저장하는 방식 개선 및 역할, 성별 Enum 설정

--- BE/User-server/Database/models.py
+++ BE/User-server/Database/models.py
@@ -6,23 +6,37 @@ Database Table Models
 """
 
 # Library
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Date, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
+from enum import Enum as BaseEnum
 
 # Create table base
 Base = declarative_base()
 
+# Enum
+class Role(BaseEnum):
+    TEST = "test"
+    SYSTEM = "system"
+    MAIN = "main"
+    SUB = "sub"
 
-class Account(Base):
+
+class Gender(BaseEnum):
+    MALE = "male"
+    FEMALE = "female"
+    OTHER = "other"
+
+
+class AccountTable(Base):
     __tablename__ = "account"
 
     id = Column(String(16), primary_key=True, nullable=False)
     email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
-    role = Column(String(16), nullable=False)
+    role = Column(Enum(Role), nullable=False)
     user_name = Column(String(32), nullable=True)
-    age = Column(Integer, nullable=True)
-    gender = Column(String(16), nullable=True)
+    birth_date = Column(Date, nullable=True)
+    gender = Column(Enum(Gender), nullable=True)
     address = Column(String(128), nullable=True)
 
     def __repr__(self):
@@ -32,7 +46,7 @@ class Account(Base):
                 f"password='{self.password}', " +
                 f"role='{self.role}', " +
                 f"user_name='{self.user_name}', " +
-                f"age='{self.age}', " +
+                f"birth_date='{self.birth_date}', " +
                 f"gender='{self.gender}', " +
                 f"address='{self.address}')>"
                 )
