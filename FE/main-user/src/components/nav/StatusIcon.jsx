4aab8e8982158cbb85523b0c5d0cafdb6bc17a8d
itdice <itdiceofficial@gmail.com>
2025-01-20 15:59:59 +0900

    :card_file_box: [Mod] 불필요한 파일 및 폴더 삭제

--- FE/main-user/src/components/nav/StatusIcon.jsx
+++
@@ -1,7 +0,0 @@
-export default function StatusIcon({ imgSrc, altSrc, status, onClickIcon }) {
-  return (
-    <button onClick={onClickIcon} className={status ? "selected" : undefined}>
-      <img src={imgSrc} alt={altSrc} />
-    </button>
-  );
-}
