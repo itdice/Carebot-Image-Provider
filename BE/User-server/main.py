fa37c767d0c1c191cc56cce46f7f4fdc015d5c74
itdice <itdiceofficial@gmail.com>
2025-01-31 09:34:06 +0900

    :sparkles: [Feat] Family 불러오기, 업데이트, 삭제 기능 구현

--- BE/User-server/main.py
+++ BE/User-server/main.py
@@ -45,6 +45,35 @@ app.add_middleware(  # type: ignore
 
 # ========== Account 부분 ==========
 
+# 가입 가능한 이메일인지 확인하는 기능
+@app.post("/accounts/check-email", status_code=status.HTTP_200_OK)
+async def check_email(email_check: EmailCheck):
+    target_email: str = email_check.email
+    email_list: list = [data["email"] for data in database.get_all_email()]
+
+    if target_email is None or target_email == "":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail={
+                "type": "no data",
+                "message": "Email is required",
+                "input": jsonable_encoder(email_check)
+            }
+        )
+    elif target_email in email_list:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={
+                "type": "already exists",
+                "message": "Email is already in use",
+                "input": jsonable_encoder(email_check)
+            }
+        )
+    else:
+        return {
+            "message": "Email is available"
+        }
+
 # 새로운 계정을 생성하는 기능
 @app.post("/accounts", status_code=status.HTTP_201_CREATED)
 async def create_account(account: Account):
@@ -177,35 +206,6 @@ async def create_account(account: Account):
             }
         )
 
-# 가입 가능한 이메일인지 확인하는 기능
-@app.post("/accounts/check-email", status_code=status.HTTP_200_OK)
-async def check_email(email_check: EmailCheck):
-    target_email: str = email_check.email
-    email_list: list = [data["email"] for data in database.get_all_email()]
-
-    if target_email is None or target_email == "":
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail={
-                "type": "no data",
-                "message": "Email is required",
-                "input": jsonable_encoder(email_check)
-            }
-        )
-    elif target_email in email_list:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail={
-                "type": "already exists",
-                "message": "Email is already in use",
-                "input": jsonable_encoder(email_check)
-            }
-        )
-    else:
-        return {
-            "message": "Email is available"
-        }
-
 # 모든 사용자 계정의 정보를 불러오는 기능
 @app.get("/accounts", status_code=status.HTTP_200_OK)
 async def get_all_accounts():
@@ -312,6 +312,7 @@ async def update_account(user_id: str, updated_account: Account):
             day=updated_account.birth_date.day
         )
 
+    # 최종적으로 변경할 데이터 생성
     total_updated_account: AccountsTable = AccountsTable(
         id=user_id,
         email=updated_account.email if updated_account.email is not None else previous_account["email"],
@@ -403,6 +404,40 @@ async def delete_account(user_id: str, checker: PasswordCheck):
 
 # ========== Family 부분 ==========
 
+# 주 사용자의 ID를 이용해 가족이 이미 생성되었는지 확인하는 기능
+@app.post("/families/check-exist", status_code=status.HTTP_200_OK)
+async def check_family_from_main_id(family_check: IDCheck):
+    # 필수 입력 조건 점검
+    if family_check.id is None or family_check.id == "":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail={
+                "type": "no data",
+                "loc": ["body", "id"],
+                "message": "Main ID is required",
+                "input": jsonable_encoder(family_check)
+            }
+        )
+
+    # 가족 정보가 존재하는지 확인
+    exist_family: str = database.main_id_to_family_id(family_check.id)
+
+    if exist_family == "":
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "type": "not found",
+                "message": "Main ID does not have a family",
+                "input": jsonable_encoder(family_check)
+            }
+        )
+    else:
+        return {
+            "message": "Family exists",
+            "result": {"family_id": exist_family}
+        }
+
+# 주 사용자를 기반으로 새로운 가족을 생성하는 기능
 @app.post("/families", status_code=status.HTTP_201_CREATED)
 async def create_family(family: Family):
     # 필수 입력 정보 점검 (Main User)
@@ -418,14 +453,28 @@ async def create_family(family: Family):
         )
 
     # 가족이 이미 생성되었는지 점검
-    exist_family: str = database.main_to_family_id(family.main_user)
+    exist_family: str = database.main_id_to_family_id(family.main_user)
 
-    if exist_family == "":
+    if exist_family:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail={
                 "type": "already exists",
                 "message": "Main user already has a family",
+                "input": jsonable_encoder(family)
+            }
+        )
+
+    # 주 사용자로 제공된 ID의 역할 점검
+    user_role: str = database.get_one_account(family.main_user)["role"]
+
+    if user_role is not Role.MAIN:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "invalid value",
+                "message": "Input user is not a main user",
+                "input": jsonable_encoder(family)
             }
         )
 
@@ -463,5 +512,175 @@ async def create_family(family: Family):
                 "type": "server error",
                 "message": "Failed to create new family",
                 "input": jsonable_encoder(family)
+            }
+        )
+
+# 모든 가족의 정보를 불러오는 기능
+@app.get("/families", status_code=status.HTTP_200_OK)
+async def get_all_families():
+    family_list: list = database.get_all_families()
+
+    if family_list:
+        return {
+            "message": "All families retrieved successfully",
+            "result": jsonable_encoder(family_list)
+        }
+    else:
+        return {
+            "message": "No families found",
+            "result": jsonable_encoder(family_list)
+        }
+
+# 한 가족 정보를 불러오는 기능
+@app.get("/families/{family_id}", status_code=status.HTTP_200_OK)
+async def get_family(family_id: str):
+    family_data: dict = database.get_one_family(family_id)
+
+    if family_data:
+        return {
+            "message": "Family retrieved successfully",
+            "result": jsonable_encoder(family_data)
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "type": "not found",
+                "message": "Family not found",
+                "input": {"family_id": family_id}
+            }
+        )
+
+# 한 가족 정보를 수정하는 기능
+@app.patch("/families/{family_id}", status_code=status.HTTP_200_OK)
+async def update_family(family_id: str, updated_family: Family):
+    previous_family: dict = database.get_one_family(family_id)
+
+    # 없는 가족 정보를 변경하려는지 확인
+    if not previous_family:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "type": "not found",
+                "message": "Family not found",
+                "input": jsonable_encoder(updated_family)
+            }
+        )
+
+
+    if updated_family.main_user:
+        # 없는 Main ID로 변경하려는지 확인
+        check_account: dict = database.get_one_account(updated_family.main_user)
+
+        if not check_account:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={
+                    "type": "not found",
+                    "message": "Main user not found",
+                    "input": jsonable_encoder(updated_family)
+                }
+            )
+
+        # 변경할 사용자의 역할 점검
+        user_role: str = check_account["role"]
+
+        if user_role is not Role.MAIN:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail={
+                    "type": "invalid value",
+                    "message": "Input user is not a main user",
+                    "input": jsonable_encoder(updated_family)
+                }
+            )
+
+
+
+    # 최종적으로 변경할 데이터 생성
+    total_updated_family: FamiliesTable = FamiliesTable(
+        id=family_id,
+        main_user=updated_family.main_user if updated_family.main_user is not None else previous_family["main_user"],
+        family_name=updated_family.family_name if updated_family.family_name is not None else previous_family["family_name"]
+    )
+
+    # 가족 정보 변경
+    result: bool = database.update_one_family(family_id, total_updated_family)
+
+    if result:
+        final_updated_family: dict = database.get_one_family(family_id)
+        return {
+            "message": "Family updated successfully",
+            "result": jsonable_encoder(final_updated_family)
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "type": "server error",
+                "message": "Failed to update family",
+                "input": jsonable_encoder(updated_family)
+            }
+        )
+
+# 한 가족을 삭제하는 기능
+@app.delete("/families/{family_id}", status_code=status.HTTP_200_OK)
+async def delete_family(family_id: str, checker: PasswordCheck):
+    previous_family: dict = database.get_one_family(family_id)
+
+    # 없는 가족을 삭제하려는지 확인
+    if not previous_family:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "type": "not found",
+                "message": "Family not found",
+                "input": {"family_id": family_id}
+            }
+        )
+
+    # 비밀번호 입력 없이 요청한 경우
+    if checker.password is None or checker.password == "":
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail={
+                "type": "no data",
+                "loc": ["body", "password"],
+                "message": "Password is required",
+                "input": {"family_id": family_id}
+            }
+        )
+
+    # 비밀번호 검증
+    input_password: str = checker.password
+    hashed_password: str = database.get_hashed_password(previous_family["main_user"])
+    is_verified: bool = verify_password(input_password, hashed_password)
+
+    if not is_verified:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail={
+                "type": "unauthorized",
+                "message": "Invalid password",
+                "input": {"family_id": family_id, "password": "<PASSWORD>"}
+            }
+        )
+
+    # 가족 삭제 진행
+    # TODO - 해당 Family에 연관된 Member 삭제 선행
+
+    final_result: bool = database.delete_one_family(family_id)
+
+    if final_result:
+        return {
+            "message": "Family deleted successfully"
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "type": "server error",
+                "message": "Failed to delete family",
+                "input": {"family_id": family_id, "password": "<PASSWORD>"}
             }
         )
