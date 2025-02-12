00711653fe79fb07b0ef673b7f9b191b4f000dd7
itdice <itdiceofficial@gmail.com>
2025-02-12 10:31:36 +0900

    :sparkles: [Feat] AI 대화 기반 키워드 분석, 심리 검사 내역 기능 연결

--- BE/User-server/External/ai.py
+++ BE/User-server/External/ai.py
@@ -38,7 +38,7 @@ async def check_connection() -> httpx.Response | None:
             response = await client.get(external_url)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to check connection with AI server.")
+        logger.critical(f"Error: Unable to check connection with AI server: {str(error)}")
         return None
 
 # ========== Status 부분 ==========
@@ -57,7 +57,7 @@ async def request_mental_status(family_id: str) -> httpx.Response | None:
             response = await client.post(external_url)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to request mental status from AI server.")
+        logger.critical(f"Error: Unable to request mental status from AI server: {str(error)}")
         return None
 
 # 날짜 범위로 정신 건강 리포트를 요청하는 기능
@@ -81,7 +81,41 @@ async def request_mental_reports(family_id: str, start: datetime, end: datetime)
             response = await client.post(external_url, json=request_data)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to request mental reports from AI server.")
+        logger.critical(f"Error: Unable to request mental reports from AI server: {str(error)}")
+        return None
+
+# 하루의 대화 키워드를 요약하는 기능
+async def request_conversation_keywords(family_id: str) -> httpx.Response | None:
+    """
+    주 사용자의 AI Chat 기록을 기반으로 대화 키워드를 요약하는 기능
+    :param family_id: 해당하는 가족의 ID
+    :return:
+    """
+    external_url = f"{AI_PATH}/generate-keyword/{family_id}"
+
+    try:
+        async with httpx.AsyncClient(timeout=set_timeout) as client:
+            response = await client.get(external_url)
+            return response
+    except httpx.RequestError as error:
+        logger.critical(f"Error: Unable to request conversation keywords from AI server: {str(error)}")
+        return None
+
+# 종합적인 심리 보고서를 요청하는 기능
+async def request_psychology_report(family_id: str, start: datetime, end: datetime) -> httpx.Response | None:
+    external_url = f"{AI_PATH}/analyze-mental-health/{family_id}"
+
+    request_data = {
+        "start_date": start.isoformat(timespec='seconds') if start else None,
+        "end_date": end.isoformat(timespec='seconds') if end else None
+    }
+
+    try:
+        async with httpx.AsyncClient(timeout=set_timeout) as client:
+            response = await client.post(external_url, json=request_data)
+            return response
+    except httpx.RequestError as error:
+        logger.critical(f"Error: Unable to request psychology report from AI server: {str(error)}")
         return None
 
 # ========== Chats 부분 ==========
@@ -108,7 +142,7 @@ async def talk_with_ai(user_id: str, message: str, session_id: str = None) -> ht
             response = await client.post(external_url, json=request_data)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to talk with AI server.")
+        logger.critical(f"Error: Unable to talk with AI server: {str(error)}")
         return None
 
 # ========== News & Weather 부분 ==========
@@ -122,7 +156,7 @@ async def korean_news() -> httpx.Response | None:
             response = await client.get(external_url)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to get news from AI server.")
+        logger.critical(f"Error: Unable to get news from AI server: {str(error)}")
         return None
 
 # 사용자의 위치의 날씨 정보를 가져오는 기는
@@ -134,5 +168,5 @@ async def korean_weather(user_id: str) -> httpx.Response | None:
             response = await client.get(external_url)
             return response
     except httpx.RequestError as error:
-        logger.critical("Error: Unable to get weather from AI server.")
+        logger.critical(f"Error: Unable to get weather from AI server: {str(error)}")
         return None
